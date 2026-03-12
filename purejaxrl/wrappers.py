55834acac96dc76283584d0d8d2453d14c362d67
xiao-wang007 <xwang0505dazzleyou@gmail.com>
2026-03-12 16:53:44 +0000

    _call_with_optional_params() in the wrapper has issue. fix it

--- purejaxrl/wrappers.py
+++ purejaxrl/wrappers.py
@@ -148,9 +148,9 @@ class BraxGymnaxWrapper:
 
 class MJXGymnaxWrapper:
     """Adapts a raw MJX env object to the Gymnax-style API used by PureJaxRL.
-    # TODO do the obs noise, action noise, domain randomization in the wrapper, 
-    # TODO controlled by config/RNG 
     """
+    # TODO do the obs noise, action noise, controlled by config/RNG 
+    
     def __init__(
         self,
         env,
